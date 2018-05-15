0887efa4633d53cc605bd54c5390f6f754df3ac2
Unknown <justinkjetton@gmail.com>
2018-05-15 09:00:15 -0500

    Change File Rendered

--- express_app/routes/index.js
+++ express_app/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: "Justin's Workspace" });
+  res.render('index', { title: "Justin's Workspace", name: "Justin" });
 });
 
 module.exports = router;
